85d34a508a29df21a99508ca02b1c9f769f78443
Leonardo Thome <158233688+LeoDev991@users.noreply.github.com>
2025-12-16 20:02:40 -0300

    Update ex003.py to calculate student average and status
    Modified ex003.py to prompt for student name and two grades, calculate the average, and print whether the student is approved or not based on the average. This enhances the script to provide more detailed feedback for students.

--- Aula01/ex003.py
+++ Aula01/ex003.py
@@ -1,4 +1,10 @@
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite outro número: '))
-s = n1 + n2
-print(f'A soma entre {n1} e {n2} é igual a {s}')
+nome = input('Qual o nome do aluno? ')
+n1 = int(input('Qual a sua primeira nota? '))
+n2 = int(input('Qual a sua segunda nota? '))
+media = (n1 + n2) / 2
+print(f'De acordo com o dados do aluno {nome}, a sua primeria nota é {n1} e a sua segunda nota é {n2} então sua média é {media}')
+
+if media < 5:
+    print('Infelizmente você está sendo reprovado até o momento')
+else:
+    print('Aprovado')
